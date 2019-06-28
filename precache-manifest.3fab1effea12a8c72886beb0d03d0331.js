7537dc536eb41e2e27d149aa1005a04d5278ffef
Aritra Chatterjee <aritrachatterjee07@gmail.com>
2019-06-28 21:53:59 +0530

    Updates

---
+++ precache-manifest.3fab1effea12a8c72886beb0d03d0331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcf298b4bb3014efcac96b4b5c948fd",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "e1c294054a650e11d0d8",
+    "url": "/smart-brain/static/css/2.11c68dcf.chunk.css"
+  },
+  {
+    "revision": "7f195fee193dec365e88",
+    "url": "/smart-brain/static/css/main.b7d199c8.chunk.css"
+  },
+  {
+    "revision": "e1c294054a650e11d0d8",
+    "url": "/smart-brain/static/js/2.729ede05.chunk.js"
+  },
+  {
+    "revision": "7f195fee193dec365e88",
+    "url": "/smart-brain/static/js/main.1c124126.chunk.js"
+  },
+  {
+    "revision": "b9b46f64ddcbca33fcc3",
+    "url": "/smart-brain/static/js/runtime~main.e63bf16e.js"
+  },
+  {
+    "revision": "7ca14f0440eb1723bf57414575fb993c",
+    "url": "/smart-brain/static/media/brain.7ca14f04.jpg"
+  }
+]);
